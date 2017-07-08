3ede70886740351c3d0d6f372e18e68ddd8b4ef8
gustavosantos1990 <gustavosisinfo2015@gmail.com>
2017-07-07 23:38:42 -0300

    Criacao de endereco.xhtml

--- build/generated-sources/ap-source-output/modelo/Autor_.java
+++ build/generated-sources/ap-source-output/modelo/Autor_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.Livro;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-07T21:39:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-07T22:50:10")
 @StaticMetamodel(Autor.class)
 public class Autor_ { 
 
--- build/generated-sources/ap-source-output/modelo/Editora_.java
+++ build/generated-sources/ap-source-output/modelo/Editora_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.Livro;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-07T21:39:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-07T22:50:10")
 @StaticMetamodel(Editora.class)
 public class Editora_ { 
 
